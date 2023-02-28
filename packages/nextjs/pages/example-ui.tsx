6c7f30fa0d54ce8659cbc5654cce1372f84e15ab
oogetyboogety <oogetyboogety@git.com>
2023-02-28 01:44:29 -0700

    basic map component

--- packages/nextjs/pages/example-ui.tsx
+++ packages/nextjs/pages/example-ui.tsx
@@ -1,7 +1,47 @@
 import type { NextPage } from "next";
 import Head from "next/head";
 import { ContractData, ContractInteraction } from "~~/components/ExampleUi";
+import { useEffect, useRef, ReactElement } from "react";
+import { Wrapper, Status } from "@googlemaps/react-wrapper";
+import Spinner from "../components/Spinner"
+const render = (status: Status): ReactElement => {
+  if (status === Status.FAILURE) return (<ErrorDialog />)
+  return <Spinner width="25" height="25" />;
+};
 
+function MyMapComponent({
+  center,
+  zoom,
+}: {
+  center: google.maps.LatLngLiteral;
+  zoom: number;
+}) {
+  const ref = useRef();
+
+  useEffect(() => {
+    new window.google.maps.Map(ref.current, {
+      center,
+      zoom,
+    });
+  });
+
+  return <div ref={ref} id="map" />;
+}
+const MyApp = () => {
+  const center = { lat: 39.771, lng: -104.979 };
+  const zoom = 4;
+
+  return (
+  <Wrapper apiKey={"AIzaSyD7BALiQ3L-rupI-cyiMmE2r5DPNlI-5Tg"} render={render}><MyMapComponent center={center} zoom={zoom} /> </Wrapper>
+  );
+};
+const ErrorDialog = () => {
+  return (
+    <div className="alert alert-danger" role="alert">
+    Failure
+    </div>
+  );
+};
 const ExampleUI: NextPage = () => {
   return (
     <>
@@ -15,6 +55,7 @@ const ExampleUI: NextPage = () => {
       <div className="grid lg:grid-cols-2 flex-grow" data-theme="exampleUi">
         <ContractInteraction />
         <ContractData />
+        <MyApp></MyApp>
       </div>
     </>
   );
