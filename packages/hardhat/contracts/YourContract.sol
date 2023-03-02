6c478964ad9345ead7b2a8a045c04527309f668e
TristanSaucy <tstevens1995u@gmail.com>
2023-03-01 21:17:49 -0700

    bugfix smart contract, clean up and format

--- packages/hardhat/contracts/YourContract.sol
+++ packages/hardhat/contracts/YourContract.sol
@@ -15,24 +15,23 @@ import "@openzeppelin/contracts/utils/Base64.sol";
 
  
 
-contract YourContract is ERC721, ERC721URIStorage{
+contract Event is ERC721, ERC721URIStorage{
 // 39.784133, -104.973355
 //truncate numbers to 6 decimal places only
 
-    uint256 public amount;
-    int256 public real_x;
-    int256 public real_y;
+    uint256 public token_supply;
+    int256 public x;
+    int256 public y;
+    uint private tokenID = 0 ; 
+    string public finalTokenURI;
 
-
-    constructor(uint256 _amount, int256 _real_x, int256 _real_y) ERC721("YourContract", "QRNFT") {
-        amount = _amount;
-        real_x = _real_x;
-        real_y = _real_y;
+    constructor(uint256 _token_supply, int256 _x, int256 _y, string memory URI) ERC721("YourContract", "QRNFT") {
+        token_supply = _token_supply;
+        x = _x;
+        y = _y;
+        finalTokenURI = URI;
     }
 
-    int private x = 39784133;
-    int private y = -104973355;
-    uint private tokenID = 0 ; 
     struct coords {
         int x;
         int y;
@@ -75,33 +74,10 @@ contract YourContract is ERC721, ERC721URIStorage{
     function mint(int real_x, int real_y) external {
         bool toggle = withinBounds(real_x, real_y);
         // only 20 available at this location
-        if(toggle == true && tokenID <=amount)
+        if(toggle == true && tokenID < token_supply)
         {
-            string memory uri = Base64.encode(
-            bytes(
-                string(
-                    abi.encodePacked(
-                        '{"name": "QR-NFT-PROTOTYPE",',
-                        '"description": "QR NFT associated with a real world location",',
-                        '"image": "', "https://ipfs.io/ipfs/QmTgqnhFBMkfT9s8PHKcdXBn1f5bG3Q5hmBaR4U6hoTvb1?filename=Chainlink_Elf.png", '",'
-                        '"attributes": [',
-                        '{',
-                            '"trait_type": "distance",',
-                            '"value": ', "test value",
-                            '}]'
-                        '}'
-                    )
-                )
-            )
-            );
-        // Create token URI
-        string memory finalTokenURI = string(
-            abi.encodePacked("data:application/json;base64,", uri)
-        );
-        _mint(msg.sender, tokenID);
-        _setTokenURI(tokenID, finalTokenURI);
-            
-            
+            _mint(msg.sender, tokenID);
+            _setTokenURI(tokenID, finalTokenURI);
             tokenID = tokenID + 1 ; 
         }
     }
@@ -122,7 +98,7 @@ contract YourContract is ERC721, ERC721URIStorage{
     }
 }
 
-contract Factory {
+contract EventHost {
     address public owner;
     address[] private contracts;
 
@@ -135,12 +111,12 @@ contract Factory {
         _;
     }
 
-    function createContract(uint256 amount, int256 real_x, int256 real_y) public onlyOwner {
-        address newContract = address(new YourContract(amount, real_x, real_y));
+    function createContract(uint256 amount, int256 real_x, int256 real_y, string memory URI) public onlyOwner {
+        address newContract = address(new Event(amount, real_x, real_y, URI));
         contracts.push(newContract);
     }
 
-    function getContracts() public view onlyOwner returns (address[] memory) {
+    function getContracts() public view returns (address[] memory) {
         return contracts;
     }
 }
--- packages/hardhat/deploy/00_deploy_your_contract.ts
+++ packages/hardhat/deploy/00_deploy_your_contract.ts
@@ -21,7 +21,7 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
   const { deployer } = await hre.getNamedAccounts();
   const { deploy } = hre.deployments;
 
-  await deploy("Factory", {
+  await deploy("EventHost", {
     from: deployer,
     // Contract constructor arguments
     //args: [deployer],
@@ -31,10 +31,15 @@ const deployYourContract: DeployFunction = async function (hre: HardhatRuntimeEn
     autoMine: true,
   });
 
-  await deploy("YourContract", {
+  await deploy("Event", {
     from: deployer,
     // Contract constructor arguments
-    args: [5, 39784133, -104973355],
+    args: [
+      5,
+      39784133,
+      -104973355,
+      "data:application/json;base64,eyJuYW1lIjogIlFSLU5GVC1QUk9UT1RZUEUiLCJkZXNjcmlwdGlvbiI6ICJRUiBORlQgYXNzb2NpYXRlZCB3aXRoIGEgcmVhbCB3b3JsZCBsb2NhdGlvbiIsImltYWdlIjogImh0dHBzOi8vaXBmcy5pby9pcGZzL1FtVGdxbmhGQk1rZlQ5czhQSEtjZFhCbjFmNWJHM1E1aG1CYVI0VTZob1R2YjE/ZmlsZW5hbWU9Q2hhaW5saW5rX0VsZi5wbmciLCJhdHRyaWJ1dGVzIjogW3sidHJhaXRfdHlwZSI6ICJkaXN0YW5jZSIsInZhbHVlIjogdGVzdCB2YWx1ZX1dfQ==",
+    ],
     log: true,
     // autoMine: can be passed to the deploy function to make the deployment process faster on local networks by
     // automatically mining the contract deployment transaction. There is no effect on live networks.
