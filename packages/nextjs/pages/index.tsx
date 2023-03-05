23b8fcfb136cdc716f47779669f5473e70f714e1
KingBodhi <77137817+KingBodhi@users.noreply.github.com>
2023-03-04 19:27:34 -0700

    Updated Index Page

--- packages/nextjs/pages/index.tsx
+++ packages/nextjs/pages/index.tsx
@@ -4,35 +4,27 @@ import { BugAntIcon, SparklesIcon } from "@heroicons/react/24/outline";
 import Link from "next/link";
 import React from "react";
 
+import Image from "next/image";
+import hero from "../public/assets/hero.png";
+import two from "../public/assets/two.png";
+import three from "../public/assets/three.png";
+import four from "../public/assets/four.png";
+const myLoader = ({ src, width, quality }) => {
+  return `https://example.com/${src}?w=${width}&q=${quality || 75}`;
+};
+
 const Home: NextPage = () => {
   return (
-    <>
-      <Head>
-        <title>Alpha Protocol</title>
-        <meta name="description" content="Peer to Peer Networking and Communication" />
-      </Head>
-      
-      <div className="flex items-center flex-col flex-grow pt-10">
-        <div className="px-5">
-          <h1 className="text-center mb-8">
-            <span className="block text-2xl mb-2">Experiance the Network</span>
-            <span className="block text-4xl font-bold">Alpha GO</span>
-          </h1>
-          <p className="text-center text-lg">
-            Peer to Peer Networking and Communication is Hard{" "}
-            <code className="italic bg-base-300 text-base font-bold"></code>
-          </p>
-          <p className="text-center text-lg">
-            So we made it <code className="bg-base-300 text-large font-bold">FUN!</code> {" "}
-            <code className="text-base text-xl font-bold">
-            <Link href="/profile" passHref className="link">
-              ENTER
-            </Link>
-            </code>
-          </p>
-        </div>
+      <div>
+      <center>
+        <Link href="/search" passHref className="link">
+          <Image src={hero} alt="Hero" width={10000} height={5000} />
+          <Image src={two} alt="Hero" width={10000} height={5000} />
+          <Image src={three} alt="Hero" width={10000} height={5000} />
+          <Image src={four} alt="Hero" width={10000} height={5000} />
+        </Link>
+      </center>
       </div>
-    </>
   );
 };
 
