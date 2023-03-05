d3784b7563cdc950593ee3c27f1d50f7810ef7de
TristanSaucy <tstevens1995u@gmail.com>
2023-03-04 18:26:43 -0700

    add prop for mint URI

--- packages/nextjs/components/ExampleUi/Mint.tsx
+++ packages/nextjs/components/ExampleUi/Mint.tsx
@@ -55,8 +55,7 @@ function Mint(props) {
       gasLimit: web3.utils.toHex(500000),
       to: contractAddress,
       data: contractInstance.methods.mint(
-        "data:application/json;base64,eyJuYW1lIjogIlFSLU5GVC1QUk9UT1RZUEUiLCJkZXNjcmlwdGlvbiI6ICJRUiBORlQgYXNzb2NpYXRlZCB3aXRoIGEgcmVhbCB3b3JsZCBsb2NhdGlvbiIsImltYWdlIjogImh0dHBzOi8vaXBmcy5pby9pcGZzL1FtVGdxbmhGQk1rZlQ5czhQSEtjZFhCbjFmNWJHM1E1aG1CYVI0VTZob1R2YjE/ZmlsZW5hbWU9Q2hhaW5saW5rX0VsZi5wbmciLCJhdHRyaWJ1dGVzIjogW3sidHJhaXRfdHlwZSI6ICJkaXN0YW5jZSIsInZhbHVlIjogdGVzdCB2YWx1ZX1dfQ=="
-
+        props.URI
       ).encodeABI()
     };
 
