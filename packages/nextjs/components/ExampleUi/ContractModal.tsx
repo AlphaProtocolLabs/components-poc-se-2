2004b6e192f0ab267d9ad18595deafe66aed1e43
Aaron Bading <aaronbading.ab@gmail.com>
2023-03-04 22:51:20 -0700

    test contract function

--- packages/nextjs/components/ExampleUi/ContractModal.tsx
+++ packages/nextjs/components/ExampleUi/ContractModal.tsx
@@ -1,4 +1,3 @@
-
 import { useEffect, useRef, ReactElement, useState } from "react";
 import { Wrapper, Status } from "@googlemaps/react-wrapper";
 import Spinner from "../Spinner";
@@ -7,31 +6,29 @@ import Mint from "./Mint";
 import { useRouter } from "next/router";
 import Image from "next/image";
 import spork from "../../public/assets/spork.png";
-function ContractModal({toggleShowModal}: {toggleShowModal: Function}) {
-    
-    return (
-
-      <div id="modal-root">
-        <Modal onClose={() => toggleShowModal()} show={toggleShowModal} title="root">
-          <div className="pt-10">
-            <center>
-              <Image src={spork} alt="Spork Castle NFT" width={300} height={300} />
-            </center>
-          </div>
-
-          <div className="pt-5">
-            <h1 className="text-center text-2xl font-bold"> SPORK CASTLE </h1>
-            <h3 className="text-center text-lg "> 0xAddress </h3>
-            <h4 className="text-center text-lg "> 4655 Humboldt St, Denver CO 80216 </h4>
-          </div>
-
-          <div className="pt-5">
+function ContractModal({ toggleShowModal }: { toggleShowModal: Function }) {
+  return (
+    <div id="modal-root">
+      <Modal onClose={() => toggleShowModal()} show={toggleShowModal} title="root">
+        <div className="pt-10">
           <center>
-              <Mint contract_address = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788" URI = "insert_string_here"/>
+            <Image src={spork} alt="Spork Castle NFT" width={300} height={300} />
           </center>
-          </div>
-        </Modal>
-      </div>
-    );
+        </div>
+
+        <div className="pt-5">
+          <h1 className="text-center text-2xl font-bold"> SPORK CASTLE </h1>
+          <h3 className="text-center text-lg "> 0xAddress </h3>
+          <h4 className="text-center text-lg "> 4655 Humboldt St, Denver CO 80216 </h4>
+        </div>
+
+        <div className="pt-5">
+          <center>
+            <Mint contract_address="0x123Ca8b94207567769D5b259E581A149D844CAF4" URI="insert_string_here" />
+          </center>
+        </div>
+      </Modal>
+    </div>
+  );
 }
 export default ContractModal;
--- packages/nextjs/components/ExampleUi/Mint.tsx
+++ packages/nextjs/components/ExampleUi/Mint.tsx
@@ -4,8 +4,6 @@ import mint_contract_abi from "../../../mint_contract_abi.json";
 
 function Mint(props) {
   const handleClick = async () => {
-    
-
     const options = {
       enableHighAccuracy: true,
       timeout: 5000,
@@ -15,68 +13,64 @@ function Mint(props) {
     function error(err) {
       console.warn(`ERROR(${err.code}): ${err.message}`);
     }
-    
+
     async function success(pos) {
       const crd = pos.coords;
-    
+
       console.log("Your current position is:");
       console.log(`Latitude : ${crd.latitude}`);
       console.log(`Longitude: ${crd.longitude}`);
       console.log(`More or less ${crd.accuracy} meters.`);
-      var web3 = new Web3(Web3.givenProvider)
+      var web3 = new Web3(Web3.givenProvider);
 
-    //if(props.coords[0] )
+      //if(props.coords[0] )
 
-    // this needs actual contract addy
-    const contractAddress = props.contract_address;
-    //const contractAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
+      // this needs actual contract addy
+      const contractAddress = props.contract_address;
+      //const contractAddress = "0x610178dA211FEF7D417bC0e6FeD39F05609AD788";
 
-    // Create contract instance
-    const contractInstance = new web3.eth.Contract(mint_contract_abi, contractAddress);
+      // Create contract instance
+      const contractInstance = new web3.eth.Contract(mint_contract_abi, contractAddress);
 
-    const coords = await contractInstance.methods.getCoords().call()
-    console.log(props.coords)
-    console.log(coords)
-    const x_diff =Math.abs(parseInt(crd.latitude*1000000) - coords[0])
-    const y_diff = Math.abs(parseInt(crd.longitude*1000000) - coords[1])
-    console.log(x_diff)
-    console.log(y_diff)
-  
-    if ( x_diff < 450 && y_diff < 450 )
-    {
-    // Set up account for signing with metamask
-    const accounts = await web3.eth.getAccounts();
-    const account = accounts[0];
+      const coords = await contractInstance.methods.getCoords().call();
+      console.log(props.coords);
+      console.log(coords);
+      const x_diff = Math.abs(parseInt(crd.latitude) - coords[0] / 1000000000000000000);
+      const y_diff = Math.abs(parseInt(crd.longitude) - coords[1] / 1000000000000000000);
+      console.log(x_diff);
+      console.log(y_diff);
+      console.log("------------");
+      console.log(coords[0]);
+      console.log(crd.latitude);
+      console.log("------------");
 
-    // Create transaction object
-    const txObject = {
-      from: account,
-      gasPrice: web3.utils.toHex(web3.utils.toWei("300", "gwei")),
-      gasLimit: web3.utils.toHex(500000),
-      to: contractAddress,
-      data: contractInstance.methods.mint(
-        props.URI
-      ).encodeABI()
-    };
+      if (x_diff < 450 && y_diff < 450) {
+        // Set up account for signing with metamask
+        const accounts = await web3.eth.getAccounts();
+        const account = accounts[0];
 
-    try {
-      // Prompt the user to sign the transaction with Metamask
-      const signedTx = await web3.eth.sendTransaction(txObject);
-      console.log(`Transaction sent: ${signedTx.transactionHash}`);
-    } catch (error) {
-      console.error(error);
+        // Create transaction object
+        const txObject = {
+          from: account,
+          gasPrice: web3.utils.toHex(web3.utils.toWei("300", "gwei")),
+          gasLimit: web3.utils.toHex(500000),
+          to: contractAddress,
+          data: contractInstance.methods.mint(props.URI).encodeABI(),
+        };
+
+        try {
+          // Prompt the user to sign the transaction with Metamask
+          const signedTx = await web3.eth.sendTransaction(txObject);
+          console.log(`Transaction sent: ${signedTx.transactionHash}`);
+        } catch (error) {
+          console.error(error);
+        }
+      } else {
+        console.log("Not close enough to redeem, sorry!");
+      }
     }
-  }
-  else
-  {
-    console.log("Not close enough to redeem, sorry!");
-  }
-    }
-    
+
     const result = navigator.geolocation.getCurrentPosition(success, error, options);
-
-    
-    
   };
 
   return (
@@ -88,6 +82,6 @@ function Mint(props) {
       </div>
     </>
   );
-};
+}
 
 export default Mint;
